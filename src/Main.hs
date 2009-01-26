f25037055737099f03eecc748491164a6740a970
Gísli Kristjánsson <gislik@hamstur.is>
2009-01-26 23:17:39 +0000

    Added cookieFixer to Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -15,12 +15,14 @@ import Controller.User
 import Controller.Atom
 import Controller.Session
 import Text.StringTemplate (newSTMP)
+import Util.CookieFixer
 
 main :: IO ()
 main = do
      putStrLn "Starting ..."
      control <- startSystemState entryPoint
      tid <- forkIO $ simpleHTTP (Conf {port=5000, validator=Nothing}) $
+                   [cookieFixer.multi $
                    [dir "users" userController] ++                    
                    [dir "blog" blogController] ++
                    [dir "atom" atomController] ++
@@ -28,7 +30,7 @@ main = do
                    [dir "proxy" [proxyServe ["google.com","*.google.com"]]] ++
                    [dir "src" [sourceServe [] "src"]] ++
                    [dirindex [withRequest void]] ++
-                   [fileServe' ["index.html"] "public"]
+                   [fileServe' ["index.html"] "public"]]
 --     putStrLn "Creating checkpoint"
 --     createCheckpoint control
      putStrLn "Started"
